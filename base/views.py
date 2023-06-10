24a154992d2b891f2d5a390115e9458210d2f15d
d1zm4as <pedrofamilia364@gmail.com>
2023-06-10 15:01:10 -0300

    some upś/everythong good

--- base/views.py
+++ base/views.py
@@ -18,5 +18,11 @@ def home(request):
     return render(request,'base/home.html',context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request,pk):
+    room = None
+
+    for x in rooms:
+        if x['id']==int(pk):
+            room =x
+    context  = {'room':room}
+    return render(request, 'base/room.html',context)
